dbc9d4844c342bd9b2cf15aab219779d5604d289
ChristianMacDonald <chrismacd412@gmail.com>
2019-06-25 23:27:14 -0400

    Added thunk to fetch characters from Star Wars API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,30 @@
 // we'll need axios
+import Axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
+export const FETCHING = 'FETCHING';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+export const fetchCharacters = () => dispatch => {
+    dispatch({ type: FETCHING });
+    Axios.get('https://swapi.co/api/people')
+        .then(result => {
+            console.log(result);
+            dispatch({
+                type: SUCCESS,
+                payload: result.data
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            dispatch({
+                type: FAILURE,
+                payload: error.data
+            });
+        });
+}
